e32e4967c6f871e042be2db3d34f1d2ce99920f8
jim199 <ntwmd4u@gmail.com>
2026-01-24 15:28:42 +0200

    Fix duplicate DbContext registration

--- backend/DeviceDiagnostics.Api/Program.cs
+++ backend/DeviceDiagnostics.Api/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
